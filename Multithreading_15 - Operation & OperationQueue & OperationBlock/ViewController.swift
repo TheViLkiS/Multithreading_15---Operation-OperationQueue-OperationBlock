40319cfc9b6ce06c5af30827820fab7893d5486d
TheViLkiS <thevilkis@gmail.com>
2023-05-30 16:43:24 +0400

    finish

--- Multithreading_15 - Operation & OperationQueue & OperationBlock/ViewController.swift
+++ Multithreading_15 - Operation & OperationQueue & OperationBlock/ViewController.swift
@@ -11,7 +11,65 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+
+        
+//        print(Thread.current)
+//
+//        let closure1 = {
+//            print("Start")
+//            print(Thread.current)
+//            print("Finish")
+//        }
+//
+//        let queue = OperationQueue()
+//        queue.addOperation(closure1)
+        
+//        print(Thread.current)
+//
+//        var result: String?
+//        let concutOperation = BlockOperation {
+//            result = "The Swift" + " " + "Developers"
+//            print(Thread.current)
+//        }
+////        concutOperation.start()
+////        print(result!)
+//
+//        let queue = OperationQueue()
+//        queue.addOperation(concutOperation)
+
+        
+//        let queue1 = OperationQueue()
+//        queue1.addOperation {
+//            print(Thread.current)
+//        }
+        
+        
+        class MyThread: Thread {
+            override func main() {
+                print("test main thread")
+                print(Thread.current)
+            }
+        }
+        
+        class OperationA: Operation {
+            override func main() {
+                print("test Operation")
+                print(Thread.current)
+            }
+        }
+        
+//        let myThread = MyThread()
+//        myThread.start()
+        
+        let myOperation = OperationA()
+//        myOperation.start()
+        
+        let queue2 = OperationQueue()
+        queue2.addOperation(myOperation)
+        
+        
     }
 
 
